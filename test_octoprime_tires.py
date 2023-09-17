097bcc03c198b02dcad32232af7a51e49d26e492
EvaLin2951 <132865370+EvaLin2951@users.noreply.github.com>
2023-09-18 04:48:22 +0800

    Add files via upload

---
+++ test_octoprime_tires.py
@@ -0,0 +1,16 @@
+import unittest
+from tires.octoprime_tires import OctoprimeTires
+
+class TestOctoprimeTires(unittest.TestCase):
+    def test_needs_service_true(self):
+        tire_wear = [1.0, 1.0, 1.0, 1.0]
+        tires = OctoprimeTires(tire_wear)
+        self.assertTrue(tires.needs_service())
+
+    def test_needs_service_false(self):
+        tire_wear = [0.1, 0.2, 1.0, 1.0]
+        tires = OctoprimeTires(tire_wear)
+        self.assertFalse(tires.needs_service())
+
+if __name__ == '__main__':
+    unittest.main()
